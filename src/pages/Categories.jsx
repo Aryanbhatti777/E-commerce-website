37ac47e1d820e9ea1bd8ba35c6abf75bb1cc08b1
Aryan <bhattiaryan777@gmail.com>
2026-03-18 14:41:17 +0530

    done

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -22,6 +22,7 @@ const Categories = () => {
         ))}
       </div>
     </section>
+    
   );
 };
 
--- src/pages/CategoryProducts.jsx
+++ src/pages/CategoryProducts.jsx
@@ -1,35 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { electronics } from "../services/Api";
 
 const CategoryProducts = () => {
   const { slug } = useParams();
-  let [data , setData] = useState()
-
-  // useEffect(
-  //   () => {
-  //     let product = electronics()
-  //     setData(product)
-  //     // console.log(data)
-  //   }
-  // )
 
   return (
-    <>
+    <div>
       <h2>{slug} Products</h2>
-
-      {/* {data.map(
-        (pro) => {
-          <ul key={pro.id}>
-            <li>pro.title</li>
-            <li>{pro.price}</li>
-            <li>{pro.description}</li>
-          </ul>
-        }
-      )} */}
-
-
-    </>
+    </div>
   );
 };
 
